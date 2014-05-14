c303037e24114be27aeb0c62e13d2c7a7f059478
Russ Olsen <russ@russolsen.com>
2014-05-14 17:15:48 -0400

    Added spec for char_stream

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require 'time'
 REPEAT = (ENV["REPEAT"] || 150).to_i
 
 RSpec.configure do |c|
-#  c.fail_fast = true
+  c.fail_fast = true
   c.filter_run_including :focused => true
   c.alias_example_to :fit, :focused => true
   c.treat_symbols_as_metadata_keys_with_true_values = true
@@ -15,6 +15,10 @@ RSpec.configure do |c|
 
   c.filter_run :focus
   c.run_all_when_everything_filtered = true
+end
+
+def io_for(s)
+  StringIO.new(s)
 end
 
 module RantlyHelpers
