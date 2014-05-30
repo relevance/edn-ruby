babc61f7d0e67efbc617bec41966e83c7f69fcf3
Russ Olsen <russ@russolsen.com>
2014-05-30 14:00:17 -0400

    Deleted old (enum related) tests, added a new basic test for reader

--- spec/edn/reader_spec.rb
+++ spec/edn/reader_spec.rb
@@ -3,18 +3,17 @@ require 'spec_helper'
 describe EDN::Reader do
   let(:reader) { EDN::Reader.new("[1 2] 3 :a {:b c} ") }
 
+  it "should read each value" do
+    reader.read.should == [1, 2]
+    reader.read.should == 3
+    reader.read.should == :a
+    reader.read.should == {:b => ~"c"}
+  end
+
   it "should respond to each" do
     reader.each do |element|
       element.should_not be_nil
     end
-  end
-
-  it "should return an Enumerator from each if no block given" do
-    reader.each.should be_a(Enumerator)
-  end
-
-  it "should respond to map" do
-    reader.map { |x| x }.should == [[1, 2], 3, :a, {:b => ~"c"}]
   end
 
   it "returns a special end of file value if asked" do
