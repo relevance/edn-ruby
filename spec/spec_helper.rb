63545bcea9d10b80de85293107aaceb3fc73a35a
Ed Porras <ed@motologic.com>
2018-07-02 16:18:22 +0200

    no need to set treat_symbols_as_metadata_keys_with_true_values to true

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,8 @@ RSpec.configure do |c|
   c.fail_fast = true
   c.filter_run_including :focused => true
   c.alias_example_to :fit, :focused => true
-  c.treat_symbols_as_metadata_keys_with_true_values = true
+  # now always set to true
+#  c.treat_symbols_as_metadata_keys_with_true_values = true
   c.run_all_when_everything_filtered = true
 
   c.filter_run :focus
