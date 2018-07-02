f8aa035cc78f0a4bbd4716fab952784106d76995
Ed Porras <ed@motologic.com>
2018-07-02 16:10:37 +0200

    update rspec usage to remove warnings

--- spec/edn_spec.rb
+++ spec/edn_spec.rb
@@ -13,14 +13,14 @@ describe EDN do
       it "reads file #{File.basename(edn_file)} correctly" do
         expected = eval(File.read(rb_file))
         actual = EDN.read(File.read(edn_file))
-        actual.should == expected
+        expect(actual).to eq(expected)
       end
 
        it "round trips the value in #{File.basename(edn_file)} correctly" do
         expected = eval(File.read(rb_file))
         actual = EDN.read(File.read(edn_file))
         round_trip = EDN.read(actual.to_edn)
-        round_trip.should == expected
+        expect(round_trip).to eq(expected)
       end
     end
   end
@@ -28,47 +28,47 @@ describe EDN do
 
   context "#read" do
     #it "reads from a stream" do
-    #  io = StringIO.new("123")
+    #  io = StringIO.new('123')
     #  EDN.read(io).should == 123
     #end
 
     #it "reads mutiple values from a stream" do
-    #  io = StringIO.new("123 456 789")
+    #  io = StringIO.new('123 456 789')
     #  EDN.read(io).should == 123
     #  EDN.read(io).should == 456
     #  EDN.read(io).should == 789
     #end
 
     it "raises an exception on eof by default" do
-      expect { EDN.read('') }.to raise_error
+      expect { EDN.read('') }.to raise_error(RuntimeError)
     end
 
     #it "allows you to specify an eof value" do
-    #  io = StringIO.new("123 456")
+    #  io = StringIO.new('123 456')
     #  EDN.read(io, :my_eof).should == 123
     #  EDN.read(io, :my_eof).should == 456
     #  EDN.read(io, :my_eof).should == :my_eof
     #end
 
     it "allows you to specify nil as an eof value" do
-      EDN.read("", nil).should == nil
+      expect(EDN.read('', nil)).to be_nil
     end
   end
 
   context "reading data" do
     it "treats carriage returns like whitespace" do
-      EDN.read("\r\n[\r\n]\r\n").should == []
-      EDN.read("\r[\r]\r\r").should == []
+      expect(EDN.read("\r\n[\r\n]\r\n")).to eq([])
+      expect(EDN.read("\r[\r]\r\r")).to eq([])
     end
 
     it "reads any valid element" do
       elements = rant(RantlyHelpers::ELEMENT)
       elements.each do |element|
         begin
-          if element == "nil"
-            EDN.read(element).should be_nil
+          if element == 'nil'
+            expect(EDN.read(element)).to be_nil
           else
-            EDN.read(element).should_not be_nil
+            expect(EDN.read(element)).to_not be_nil
           end
         rescue Exception => ex
           puts "Bad element: #{element}"
@@ -80,8 +80,8 @@ describe EDN do
 
   context "#register" do
     it "uses the identity function when no handler is given" do
-      EDN.register "some/tag"
-      EDN.read("#some/tag {}").class.should == Hash
+      EDN.register 'some/tag'
+      expect(EDN.read('#some/tag {}').class).to eq(Hash)
     end
   end
 
@@ -104,9 +104,9 @@ describe EDN do
       elements = rant(RantlyHelpers::ELEMENT)
       elements.each do |element|
         ruby_element = EDN.read(element)
-        ruby_element.should == EDN.read(ruby_element.to_edn)
+        expect(ruby_element).to eq(EDN.read(ruby_element.to_edn))
         if ruby_element.respond_to?(:metadata)
-          ruby_element.metadata.should == EDN.read(ruby_element.to_edn).metadata
+          expect(ruby_element.metadata).to eq(EDN.read(ruby_element.to_edn).metadata)
         end
       end
     end
